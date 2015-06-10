676a292a568a30191708e00b0c6ffd2e79e00fa2
danielglatstein <danielglatstein@gmail.com>
2015-06-10 14:32:22 -0400

    add 10><10 meth

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,6 +12,11 @@ def swap_elements(array)
     array[0], array[1], array[2] = array[0], array[2], array [1]
 end
 
+def swap_elements_from_to(array, index, destination_index)
+    array[index], array[destination_index] = array[destination_index], array[index]
+    array
+end
+
 def reverse_array(array)
     array.reverse
 end
@@ -22,3 +27,16 @@ def kesha_maker(array)
     end
 end
 
+def greater_and_less_than_10(array)
+    sorted_hash = Hash.new
+    sorted_hash["greater_than_10"] = [ ]
+    sorted_hash["less_than_10"] = [ ]
+    array.each do |num|
+        if num > 10
+            sorted_hash["greater_than_10"] << num
+        else num < 10
+            sorted_hash["less_than_10"] << num
+        end
+    end
+    sorted_hash
+end
